6bbb2c11bb1fe84dce41fcc80da9e9380dca22ad
Masunori <minhducduong128@gmail.com>
2025-01-21 23:11:27 +0800

    Rename and add greeting to main class.

---
+++ src/main/java/Mirai.java
@@ -0,0 +1,11 @@
+public class Mirai {
+    public static void main(String[] args) {
+        String greeting = "____________________________________________________________\n" +
+                " Hello! I'm Mirai\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(greeting);
+    }
+}
